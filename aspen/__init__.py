6c54e9b497d22cfa5d57d66dffb84ee9054f7fbb
Chad Whitacre <chad@zetaweb.com>
2009-10-03 04:41:05 +0000

    I brought over configuration, including tests. Configuration is no longer global! (issue 137). Also, the default port is now 5370. :^)

--- aspen/__init__.py
+++ aspen/__init__.py
@@ -1,9 +1,17 @@
+import sys
+
+
+# Set for use in aspen.configuration.
+__version__ = '~~VERSION~~'
+WINDOWS = 'win32' in sys.platform
+
+
+from aspen.configuration import Configuration
+
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from tornado.web import Application, RequestHandler
 
-
-__version__ = '~~VERSION~~'
 
 
 
@@ -16,10 +24,21 @@ class SimplateHandler(RequestHandler):
 
     post = get
 
+
+
+
+
  
-def main():
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+    configuration = Configuration(argv)
+    #TODO make configuration available down in simplates? E.g.:
+    # config.paths.root
+
     app = Application([('^.*$', SimplateHandler)])
     server = HTTPServer(app)
-    server.listen(5370)
+    server.listen(configuration.address[1]) #TODO what about AF_UNIX?
     loop = IOLoop.instance()
     loop.start()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,18 +1,23 @@
-import aspen
 from multiprocessing import Process
 
+import aspen
 from tornado.httpclient import HTTPClient
 
 
 class AspenRunning:
 
     def __enter__(self):
+        #TODO can't use Process here, because it is not designed for tweaking
+        # cli args. So we call aspen.main with nosetext -sx args and Conf 
+        # breaks.
         self.proc = Process(target=aspen.main)
         self.proc.start()
 
     def __exit__(self, *exc):
         self.proc.terminate() # unclean!
             #TODO: bring over robust proc testing with proc mgmt (daemon, etc.)
+            # would be swell to update that for multiprocessing?
+            # nah, leave it with subprocess for usage on < 2.6
         self.proc.join()
 
 
