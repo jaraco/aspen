b42fe26aed2ed87c18fe959999ec35d653bb13f0
Chad Whitacre <chad@zetaweb.com>
2009-10-03 08:19:42 +0000

    Brought over server and the rest of main() (with tests). Daemonization and restarting are working.

--- aspen/__init__.py
+++ aspen/__init__.py
@@ -1,4 +1,8 @@
+import atexit
+import logging
+import os
 import sys
+import traceback
 
 
 # Set for use in aspen.configuration.
@@ -7,45 +11,101 @@ WINDOWS = 'win32' in sys.platform
 
 
 from aspen.configuration import Configuration, ConfigurationError, usage
-
-from tornado.httpserver import HTTPServer
-from tornado.ioloop import IOLoop
-from tornado.web import Application, RequestHandler
+from aspen.ipc import restarter
+from aspen.server import Server
 
 
+log = logging.getLogger('aspen')
 
 
-class SimplateHandler(RequestHandler):
+# Helper
+# ======
 
-    def get(self):
-        """Serve the request by processing a simplate.
-        """
-        self.write('Greetings, program!')
+def absolutize_root(argv):
+    """Absolutize any --root given in argv.
 
-    post = get
+    We need to absolutize any root path, because when we daemonize we chdir in 
+    the daemon/parent, so if --root is relative it will break in the child.
+
+    We only run this when we are demonizing. It will have been validated by 
+    OptParse by then.
+
+    """
+    for i in range(len(argv)):
+        val = argv[i]
+        if val in ('-r', '--root'):
+            root = argv[i+1]
+            argv[i+1] = os.path.realpath(root)
+            break
+        elif val.startswith('-r'):
+            root = val[len('-r'):]
+            argv[i] = '-r%s' % os.path.realpath(root)
+            break
+        elif val.startswith('--root='):
+            root = val[len('--root='):]
+            argv[i] = '--root=%s' % os.path.realpath(root)
+            break
+    return argv
 
 
+# Main
+# ====
+# Unsafe and safe.
+
+def _main(configuration):
+    """Given a configuration object, do daemony things and go into a loop.
+    """
+    if restarter.PARENT:
+        argv = sys.argv[:]
+        if configuration.daemon is not None:
+            argv = absolutize_root(argv)
+            configuration.daemon.drive() # will daemonize or raise SystemExit
+        restarter.loop(argv)
+    else:
+        assert restarter.CHILD # sanity check
+        if configuration.daemon is not None:
+            configuration.pidfile.write() # only in CHILD of daemon
+            atexit.register(configuration.pidfile.remove)
+        server = Server(configuration)
+        server.start()
 
 
-
- 
 def main(argv=None):
+    """Configure safely and then run the main loop safely.
+    """
+
     if argv is None:
         argv = sys.argv
 
-
-    #TODO make configuration available down in simplates? E.g.:
-    # config.paths.root
     try:
         configuration = Configuration(argv)
+        #TODO make configuration available down in simplates? E.g.:
+        # config.paths.root
     except ConfigurationError, err:
         print >> sys.stderr, usage
         print >> sys.stderr, err.msg
         sys.exit(2)
 
+    try:
+        _main(configuration)
+    except SystemExit, exc:
+        if exc.code == 0:
+            log_func = log.debug
+        if exc.code != 0:
+            log_func = log.info
+        
+        tb = sys.exc_info()[2]
+        while tb.tb_next is not None:
+            tb = tb.tb_next
+        frame = tb.tb_frame
+        filename = os.path.basename(frame.f_code.co_filename)
+        location = "%s:%s" % (filename, frame.f_lineno)
+        log_func("exiting with exit code %d (from %s)" % (exc.code, location))
 
-    app = Application([('^.*$', SimplateHandler)])
-    server = HTTPServer(app)
-    server.listen(configuration.address[1]) #TODO what about AF_UNIX?
-    loop = IOLoop.instance()
-    loop.start()
+        raise # fyi, without this child wasn't terminating
+
+    except KeyboardInterrupt:
+        pass
+    except:
+        log.critical(traceback.format_exc())
+        sys.exit(1)
---
+++ aspen/server.py
@@ -0,0 +1,118 @@
+import atexit
+import logging
+import os
+import socket
+import sys
+import traceback
+
+import aspen
+from aspen.ipc import restarter
+from tornado.httpserver import HTTPServer
+from tornado.web import HTTPError
+from tornado.ioloop import IOLoop
+from tornado.web import Application, RequestHandler
+
+
+log = logging.getLogger('aspen.server')
+
+
+class SimplateHandler(RequestHandler):
+    #TODO move this to its own module
+
+    def get(self):
+        """Serve the request by processing a simplate.
+        """
+        #TODO barebones handler for some test_server.py tests
+        docroot = os.path.join(self.application.configuration.root, 'www')
+        fspath = os.path.join(docroot, self.request.path[1:])
+        if os.path.isdir(fspath):
+            fspath = os.path.join(fspath, 'index.html')
+        if os.path.isfile(fspath):
+            self.write(open(fspath).read())
+        else:
+            raise HTTPError(404)
+
+    post = put = delete = head = get
+
+
+class Server:
+
+    def __init__(self, configuration):
+        """Extend to take a Configuration object.
+        """
+        self.configuration = configuration
+        self.protocol = "HTTP/%s" % configuration.http_version
+        self.version = "Aspen/%s" % aspen.__version__
+        atexit.register(self.stop)
+   
+
+    def start(self):
+        """Start the server with filesystem monitoring.
+        """
+        log.warn("starting on %s" % str(self.configuration.address))
+   
+        if aspen.mode.DEBDEV:
+            log.info("configuring filesystem monitor")
+            for path in ( os.path.join('etc', 'aspen.conf')
+                        , os.path.join('etc', 'logging.conf')
+                         ):
+                if os.path.isfile(path):
+                    restarter.monitor(path)
+            restarter.start_monitoring()
+
+
+        # Now it's time for the Tornado API.
+        # ==================================
+
+        app = Application( [('^.*$', SimplateHandler)]
+                         , debug=aspen.mode.DEBDEV
+                          )
+        app.configuration = self.configuration
+        self.server = HTTPServer(app)
+        self.server.listen(self.configuration.address[1]) #TODO AF_UNIX?
+        loop = IOLoop.instance()
+        loop.start()
+
+
+    def stop(self):
+        """Clean up and exit.
+
+        Tornado does not close HTTPServer's socket itself, relying on garbage
+        collection. This works fine when the program is ending anyway, but in 
+        testing we need to be able to rebind to the socket again in the same
+        process. So we close it when the loop is over.
+
+        """
+        log.debug("cleaning up server")
+        sys.stdout.flush()
+
+        loop = IOLoop.instance()
+        loop.add_callback(lambda: self.server._socket.close())
+        loop.stop()
+
+        if not aspen.WINDOWS: 
+            if self.configuration.sockfam == socket.AF_UNIX: # clean up socket
+                try:
+                    os.remove(configuration.address)
+                except EnvironmentError, exc:
+                    log.error("error removing socket:", exc.strerror)
+
+        # pidfile removed in __init__.py:_main
+        # restarter stopped in ipc/restarter.py:_atexit
+
+
+# Support restarting when we are restarter.CHILD.
+# ===============================================
+# Note that when using aspen._main, Server is only ever instantiated within 
+# restarter.CHILD.
+
+if restarter.CHILD:
+
+    def check_restart():
+        if restarter.should_restart():
+            log.info("restarting")
+            raise SystemExit(75) # will trigger self.stop via atexit
+
+    loop = IOLoop.instance() 
+    loop.add_callback(check_restart)
+
