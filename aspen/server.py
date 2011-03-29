9a7f2e3973f0298cbc63e9ed09eaa89fa485edd2
Chad Whitacre <chad@zetaweb.com>
2011-03-29 08:37:34 -0400

    This is me flailing. I just ordered W. Richard Stephens.

--- aspen/server.py
+++ aspen/server.py
@@ -1,69 +1,107 @@
 #!/usr/bin/env python
-import Queue
-import diesel
+import sys
 import socket
 import threading
 import time
+from Queue import Queue as ThreadQueue
+
+import diesel
+
+
+def parse_request():
+    #rest = receive(9)
+    #print rest
+    print "HELP!!!"
+    yield
+
 
 
 # Cooperative
 # ===========
 
-def cooperate(conn):
-
-    conn.send('SELECTED ' + data)
-    conn.close()
+def cooperate(data, conn, addr):
+    print "now recv the rest of %s" % data
+    conn.setblocking(False)
+    print "cooperating?"
+    c = diesel.Connection(conn, addr)
+    print "still cooperating?"
+    loop = diesel.Loop(parse_request, addr)
+    print "*still* cooperating?"
+    loop.connection_stack.append(c)
+    print "cooperating now?"
+    app.add_loop(loop)
 
 
 # Blocking
 # ========
 
-queue_blocking = Queue.Queue()
-
-def do_work(conn, addr):
-    data = conn.recv(1024)
-    if data.startswith('CHEESE'):
-        conn.setblocking(False)
-        app.hub.register(conn, cooperate)
-    else:
-        conn.send(data)
-        conn.close()
-
-def worker():
-    while True:
-        work = queue_blocking.get()
-        do_work(*work)
+#thread_queue = ThreadQueue()
+#
+#def do_work(conn, addr):
+#    print "trying to do work"
+#    data = conn.recv(1024)
+#    print "processing %s" % data
+#    if data[0] in 'aeiouy':
+#        print "trying to cooperate"
+#        cooperate(data, conn, addr)
+#    else:
+#        conn.send(data)
+#        conn.close()
+#
+#def worker():
+#    while True:
+#        id = threading.current_thread().ident
+#        print "%d trying to find work" % id
+#        work = thread_queue.get()
+#        print "%d found work" % id
+#        do_work(*work)
 
 
 # Server
 # ======
 
 def accept_connections():
-    print "accepting connections on 5370"
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(('', 5370))
-    s.listen(1)
-    while 1:
-        conn, addr = s.accept()
-        queue_blocking.put((conn, addr))
+    def _thrash():
+        print "accepting connections on 5370"
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(('', 5370))
+        sock.listen(1)
+        while True:
+            print "trying to accept a connection"
+            conn, addr = sock.accept()
+            data = conn.recv(1)
+            print "sending %s work to cooperate" % data
+            cooperate(data, conn, addr)
+            #thread_queue.put((conn, addr))
+    try:
+        _thrash()
+    except:
+        import traceback
+        traceback.print_exc()
+        print 0
+        for i in range(20):
+            sys.stdout.write("\rrestarting in %2d" % (20 - i))
+            sys.stdout.flush()
+            time.sleep(1)
+        raise SystemExit
 
 app = None
 def main():
     global app
 
-    pool = []
-    for i in range(3):
-        thread = threading.Thread(None, worker)
-        thread.daemon = True
-        pool.append(thread)
-        thread.start()
+    #pool = []
+    #for i in range(1):
+    #    thread = threading.Thread(None, worker)
+    #    thread.daemon = True
+    #    pool.append(thread)
+    #    thread.start()
     
     master_thread = threading.Thread(None, accept_connections)
     master_thread.daemon = True
     master_thread.start()
-
+    
     app = diesel.Application()
-    app.add_loop(diesel.Loop(cooperate))
     app.run()
 
 
--- aspen/tests/client.py
+++ aspen/tests/client.py
@@ -1,11 +1,49 @@
 #!/usr/bin/env python
 import socket
+import string
 import sys
+import threading
+import time
+import random
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('localhost', 5370))
-s.send(" ".join(sys.argv[1:]))
-data = s.recv(1024)
-s.close()
-print 'Received', repr(data)
+def gentokens():
+    yield "baby"
+    yield "adam"
+    while True:
+        yield ''.join(random.sample(string.lowercase, 10))
+tokens = gentokens()
+
+def hit(expected):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(('localhost', 5370))
+    s.send(expected)
+    actual = s.recv(1024)
+    s.close()
+    assert actual == expected, actual
+    print actual
+
+def loop():
+    while True:
+        token = tokens.next()
+        hit(token)
+        time.sleep(random.random())
+
+def main():
+    for i in range(3):
+        t = threading.Thread(None, loop)
+        t.daemon = True
+        t.start()
+
+    while True:
+        time.sleep(1)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        hit(' '.join(sys.argv[1:]))
+    else:
+        try:
+            main()
+        except KeyboardInterrupt:
+            pass
+
